ff9334602678ab4276cff3e536792454308da34c
David Higuita <david.higuita.75@gmail.com>
2019-07-24 14:00:30 -0500

    Added react and react-dom and initial config

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log('RJS Avanzado');
+import React from 'react';
+import reactDOM from 'react-dom';
+
+const App = () => (
+  <div className="app">
+    <h1>APP</h1>
+  </div>
+);
+
+reactDOM.render(
+  <App />,
+  document.querySelector('#app')
+)
